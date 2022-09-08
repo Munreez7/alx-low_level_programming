ba3f08e44ce2be165de579fdc71d4f97612fe663
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 02:04:10 -0700

    "Bye"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle,followed by a new line.
- * return: is always 0.
+ * main - prints "Programming is like building a multilingual
+ * puzzle,followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
 {
