8645abfe37850a2fec46862b7de042c7cc2d282c
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 17:17:47 +0000

    "modified 101-quote"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,12 @@
 #include <unistd.h>
  /**
   * Main-entry point
-  * Description-program that prints exactly and that piece of art is useful"\n
+  * 
   * Return: Always (1)(Success)
   */
-	int main(void)
-	{ /*outline what is to be printed*/
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void)
+{       
+        char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 write(2, str1, 59);
 	return (1);
 }
