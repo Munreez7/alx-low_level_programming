f67df5e7547cad68dbca0572d2c3e8763e6f4da4
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 02:21:13 -0700

    "Final Final"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints the outlined text
+ * Return: Always 0 for success
+ */
 int main(void)
-	/**
-	 * Main - prints the outlined text
-	 * Return: Always 0 for success
-	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
