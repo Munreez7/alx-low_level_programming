908a0e6c7675fea5e71ac9fe54730a587749fd13
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 02:18:27 -0700

    "Final Commit"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
+	/**
+	 * Main - prints the outlined text
+	 * Return: Always 0 for success
+	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
