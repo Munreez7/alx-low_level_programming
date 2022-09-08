b4c5e7902746b494bdf064ce709b5043c0482ffe
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 14:36:42 +0000

    "final"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,12 @@
  */
 #include <unistd.h>
  /**
-  * Main - entry point
-  * Description - program that prints exactly and that piece of art is useful" 
-  * - Dora Korpar, 2015-10-19, followed by a new line,
-  * Return: Always 1 (Success)
+  * Main-entry point
+  * Description-program that prints exactly and that piece of art is useful"\n
+  * Return: Always (1)(Success)
   */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	int main(void)
+	{ /*outline what is to be printed*/
+	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
