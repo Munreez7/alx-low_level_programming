58d298fbd6522708acc27e7399c02a48e2e85ab3
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 01:26:34 -0700

    "Betty Modif"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 /*
- * File: 4-puts.c
- * Auth: Munreez7
+ * File name: 4-puts.c
+ * Author: Munreez7
  */
-
 #include <stdio.h>
-
-/**
- * main - prints the statement outline
- * return always 0
- * */
-
+/*
+ * Program prints the outlined text
+ * return is always 0
+ */
 int main(void)
 { 
 	char str[] = "\"Programming is like building a multilingual puzzle";		
