7c8ebb1483a32d42d390f058dda15af72279a4e3
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 17:25:35 +0000

    "101-quote.c"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,11 @@
 #include <unistd.h>
  /**
   * Main-entry point
-  * 
   * Return: Always (1)(Success)
   */
 int main(void)
-{       
-        char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	 write(2, str1, 59);
+{ char str1[] = "and that piece of art is useful\n
+- Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
 	return (1);
 }
