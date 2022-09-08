3a57f0993a00c72a0764385d67008fc5cf134654
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 01:16:29 -0700

    "accomodating betty style"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: Munreez7
+ */
+
 #include <stdio.h>
+
+/**
+ * main - prints the statement outline
+ * return always 0
+ * */
 
 int main(void)
 { 
-	char str[] = "\"Programming is like building a multilingual puzzle";
-		
+	char str[] = "\"Programming is like building a multilingual puzzle";		
 		puts(str);
-
-
 	return (0);
 }
