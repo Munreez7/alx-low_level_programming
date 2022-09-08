2f9ba802544063844e90212e06ea016e0f7ad34f
Munreez7 <godwinmunroe1@gmail.com>
2022-09-08 01:37:57 -0700

    "Betty modified"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
  * return is always 0
  */
 int main(void)
-{ 
-	char str[] = "\"Programming is like building a multilingual puzzle";		
-		puts(str);
-	return (0);
+{
+	puts;("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
