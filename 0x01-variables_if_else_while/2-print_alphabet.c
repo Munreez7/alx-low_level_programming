db8723970638cdafeabbea7620ea69d2d2caecf3
Munreez7 <godwinmunroe1@gmail.com>
2022-09-09 13:51:08 +0000

    "2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program that prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+
+	return (0);
+}
