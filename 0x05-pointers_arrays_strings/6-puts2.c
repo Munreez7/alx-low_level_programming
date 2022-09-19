89a3f30dd888cb59b94d068bfb174abb10d640ac
Munreez7 <111263992+Munreez7@users.noreply.github.com>
2022-09-19 18:16:05 -0400

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,18 @@
 
 void puts2(char *str)
 {
-	int i;
-	int len;
+	int i, len;
 
 	len = 0;
 
-	while (len != '\0')
+	while (str[len] != '\0')
+	{
 		len++;
-
+	}
 	for (i = 0; i < len; i += 2)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
