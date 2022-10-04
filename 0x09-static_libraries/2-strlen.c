460095e63368543f88fa0bb9139e8af73146b63d
Munreez7 <godwinmunroe1@gmail.com>
2022-10-04 03:38:00 -0400

    Static Libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s:string
+ * Return: returns length.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
